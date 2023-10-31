fd2b81954049b25b4fb7ce00427323539a68a1c3
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-31 15:11:39 -0700

    Correct schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id),
