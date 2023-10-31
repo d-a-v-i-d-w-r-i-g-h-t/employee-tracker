31030ca667e7c380d891a520d6e4466f83f4a9d9
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-31 16:09:48 -0700

    Add initial menu choices

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2");
 
 const welcome = require('./lib/welcome.js');
-const userInput = require('./lib/userInput.js');
 const mainMenu = require("./lib/mainMenu.js");
 
 function main() {
